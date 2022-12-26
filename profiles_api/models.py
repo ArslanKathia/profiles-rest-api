89e0ab3caf2dc3bd6f987c8dd94925e186222fc8
ArslanKathia <arslanmaqbool.ciit@gmail.com>
2022-12-26 22:18:05 +0500

    user profile django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -15,7 +15,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email,name=name)
         user.set_password(password)
         user.save(using=self._db)
-        return 
+        return user
     
     def create_superuser(self,email,name,password):
         """create superuser from given details"""
